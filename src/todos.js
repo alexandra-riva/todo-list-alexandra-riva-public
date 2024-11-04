5cfbd6f212e887d6a6086e0473cfb37b795ee879
Alexandra <alexandra.riva@hyperisland.se>
2024-11-04 09:54:58 +0100

    Final

--- src/todos.js
+++
@@ -1,7 +0,0 @@
-export function addTodo(todo) {
-    const todoList = document.getElementById('todo-list');
-    const todoItem = document.createElement('li');
-    todoItem.className = 'todo-item';
-    todoItem.innerHTML = `<span>${todo.title}</span>`; 
-    todoList.appendChild(todoItem);
-}
